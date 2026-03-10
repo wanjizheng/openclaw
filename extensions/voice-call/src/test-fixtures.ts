c83cbf6e7e4c2b4307af5df0c69b98b64b60d1d4
Jizheng <wanjizheng@gmail.com>
2026-03-10 11:46:51 +0000

    fix vioce-mail issue when make phone call

--- extensions/voice-call/src/test-fixtures.ts
+++ extensions/voice-call/src/test-fixtures.ts
@@ -10,7 +10,7 @@ export function createVoiceCallBaseConfig(params?: {
     fromNumber: "+15550001234",
     inboundPolicy: "disabled",
     allowFrom: [],
-    outbound: { defaultMode: "notify", notifyHangupDelaySec: 3 },
+    outbound: { defaultMode: "notify", notifyHangupDelaySec: 3, twilioAmdMode: "off" },
     maxDurationSeconds: 300,
     staleCallReaperSeconds: 600,
     silenceTimeoutMs: 800,
